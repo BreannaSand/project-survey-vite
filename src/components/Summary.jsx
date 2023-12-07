9c646826ccfb52e1e0984a56a8fae5a6133436f9
Breanna Sand <breanna.sand8@gmail.com>
2023-12-07 11:20:33 +0100

    trouble shooting

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,25 +1,33 @@
-//import { useState } from "react";
+import { useState } from "react";
 
 export const Summary = ({ attraction, day, time }) => {
+    const [isVisible, setIsVisible] = useState(true);
+    //const [buttonClicked, setButtonClicked] = useState(false);
 
+    const handleButtonClick = () => {
+        //setButtonClicked(true);
+        // Toggle the visibility of the div
+        setIsVisible(!isVisible);
+    };
 
     console.log({ attraction })
     console.log({ day })
     console.log({ time })
     return (
+        <div className="submitButton">
+            <button type="button" onClick={handleButtonClick}>Submit</button>
+            {/*buttonClicked && */}
 
-        <div className="summary-wrap">
-            <h2>Your Booking Information</h2>
-            <p>
-
-                You have booked a fast track pass for {attraction} to use this {time} {day}.
-                <h2>Pass #67</h2>
-
-
-            </p>
-
+            {isVisible && (
+                <div className="summary-wrap">
+                    <h2>Your Booking Information</h2>
+                    <p>
+                        You have booked a fast track pass for {attraction} to use this {time} {day}.
+                        <h2>Pass #67</h2>
+                    </p>
+                </div>
+            )}
         </div>
-
 
     );
 };
